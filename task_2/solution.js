98474e1483838a1247960f64df4451d314f2862c
natalya evdokimova <asfarmaenm@mail.ru>
2022-12-21 17:57:02 +0500

    Изменения 2.3

--- task_2/solution.js
+++ task_2/solution.js
@@ -57,16 +57,17 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
-    // создайте переменную totalSum
+     let totalSum ;// создайте переменную totalSum
 
-    // присвойте totalSum значение productsSum
-    // уменьшите totalSum на discountSum
+     totalSum = productsSum ;// присвойте totalSum значение productsSum
+     totalSum -= discountSum ; // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
-    // прибавьте к totalSum значение shippingSum
+     totalSum += shippingSum ;// прибавьте к totalSum значение shippingSum
 
-    // создайте переменную freeShipping
+     let freeShipping ;
+     freeShipping = !shippingSum ;// создайте переменную freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
 

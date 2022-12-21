c8eb903f0fc395816abc9354f54a10f17f89f724
natalya evdokimova <asfarmaenm@mail.ru>
2022-12-21 17:39:55 +0500

    Изминения 2.2.

--- task_2/solution.js
+++ task_2/solution.js
@@ -36,9 +36,13 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
-    // создайте переменную discountSum
+     let discountSum;    // создайте переменную discountSum
 
-    // если productsSum больше или равно discountMinSum,
+     if (productsSum >= discountMinSum ) {
+        discountSum = discountPart * productsSum / 100 ;
+     } else {
+        discountSum = 0 ;
+     }// если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
 
